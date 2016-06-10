7f9cb7241315b59294b68c4038ebf374b53b1163
jamtot <jongrammor@gmail.com>
2016-06-10 02:06:15 +0100

    Added a problem where you have to find out the grade Benny needs to get to get her average high enough to get her iTone7.

---
+++ Problems/Benny And Her School Reports/reports.py
@@ -0,0 +1,17 @@
+def needed(args):
+    N,M,X=args[0],args[1],args[2]
+    A=map(int,raw_input().split())
+    #avg=sum(A)/float(N)
+    avg=0
+    for i in xrange(N):
+        avg+=A[i]
+    needed=(X*(N+1))-avg
+    if needed>M:
+        return 'Impossible'
+    else:
+        return abs(needed)
+    
+
+if __name__ == "__main__":
+    for tc in xrange(int(raw_input())):
+        print needed(map(int,raw_input().split()))
